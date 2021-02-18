35c4085fa5880bc6ebadd18bd11b8b213d476936
Ravi Ranjan <raviranjan3570@gmail.com>
2021-02-18 21:33:28 +0530

    added click listeners

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews : List<View> =
+            listOf(box_one, box_two, box_three, box_four, box_five, constraint_layout)
+
+            for (item in clickableViews){
+                item.setOnClickListener { makeColored(it) }
+            }
+    }
+
+    private fun makeColored(view : View) {
+        when(view.id){
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
